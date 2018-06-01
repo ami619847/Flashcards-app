cbf9612103da31d6de74926ebb5cdc6b96322bf1
RichardKorrel <richard.korrel1@gmail.com>
2018-06-01 12:21:36 +0200

    show random q and a

--- main.js
+++ main.js
@@ -7,8 +7,29 @@ var questionId = document.getElementById('flashcardQuestion');
 // select the document p element by id flashcardAnswer
 var answerId = document.getElementById('flashcardAnswer');
 
+/*
 flashcards.forEach(function(flashcard){
   questionId.innerHTML=flashcard[0];
   answerId.innerHTML=flashcard[1];
 
 })
+*/
+
+// declare function to generate a integer between 0 and max
+function getRandomInt(max) {
+  return Math.floor(Math.random() * Math.floor(max));
+}
+
+//console.log(getRandomInt(flashcards.length))
+
+// declare function to show a flashcard question and answer
+function showRandomFlashcard() {
+  let randomFlashcardIndex=getRandomInt(flashcards.length);
+  console.log('random number =' + randomFlashcardIndex);
+  //let flashcardQuestion=flashcards[randomFlashcardIndex];
+  questionId.innerHTML=flashcards[randomFlashcardIndex][0];
+  answerId.innerHTML=flashcards[randomFlashcardIndex][1];
+}
+
+// call the showRandomFlashcard fuction
+showRandomFlashcard()
