500d4e19805be35a71649771e29372e1853e43cb
ARYUNAP <AMI619847@GMAIL.com>
2018-06-01 15:56:39 +0200

    everything in one page

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+// declare array for the flash card questions and answers
+let flashcards = [['question1','answer1'],['question2','answer2']];
 let questionIndex;
 
 // select the document p element by id flashcardQuestion
@@ -76,5 +78,27 @@ var nextFlashcardAnswerId = document.getElementById('nextFlashcardAnswer');
 nextFlashcardAnswerId.onclick = function() {
     showFlashcardAnswer()
 }
+
+// declare function to save the flashcard answer for the question
+function saveFlashcard() {
+  let newQuestion = document.getElementById('newFlashcardQuestion');
+  console.log(newQuestion.value);
+  let newAnswer = document.getElementById('newFlashcardAnswer');
+  console.log(newAnswer.value);
+  flashcards.push([newQuestion.value,newAnswer.value]);
+  console.log(flashcards);
+
+}
+
+// execute the code
+
+// select the document page 2 button element by id newFlashcard
+var newFlashcardId = document.getElementById('newFlashcard');
+
+// Save new flashcard question and answer on button click
+newFlashcardId.onclick = function() {
+    saveFlashcard()
+}
+
 
 console.log(flashcards);
